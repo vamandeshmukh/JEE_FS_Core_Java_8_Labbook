acc1ffea933d5ada95df0ba821e8f944461edc51
vamandeshmukh <vamandeshmukh@gmail.com>
2021-06-14 17:12:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JEE_FS_Core_Java_8_Labbook
+ cg core java 8 labbook exercises
---
+++ src/com/cg/lab1/ex1/demo/App.java
@@ -0,0 +1,25 @@
+package com.cg.lab1.ex1.demo;
+
+/*
+ * Exercise 1: Create a method to find the sum of the cubes of the digits of an n digit number
+ */
+
+public class App {
+
+	public static void main(String[] args) {
+		int input = 12345;
+		int output = App.findSumOfCubes(input);
+		System.out.println(input);
+		System.out.println(output);
+	}
+
+	public static int findSumOfCubes(int number) {
+		int sum = 0;
+		while (number > 0) {
+			int digit = number % 10;
+			sum += digit * digit * digit;
+			number /= 10;
+		}
+		return sum;
+	}
+}
